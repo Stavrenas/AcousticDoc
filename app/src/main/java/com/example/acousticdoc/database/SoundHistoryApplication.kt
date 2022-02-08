5c9b3daf5f3638e05cdb20d7a2ce159623b00b52
Stavrenas <stavrina25@gmail.com>
2022-02-09 00:11:10 +0200

    change + to microphone icon

--- app/src/main/java/com/example/acousticdoc/database/SoundHistoryApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.acousticdoc.database
-
-import android.app.Application
-
-class SoundHistoryApplication: Application() {
-    val database by lazy { SoundHistoryDatabase.getDatabase(this) }
-    val repository by lazy { SoundHistoryRepository(database.soundHistoryDatabaseDao()) }
-}
